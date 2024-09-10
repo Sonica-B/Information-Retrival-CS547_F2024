f53075224df4f6d9eda3239d4d5fa8c9a30256e9
Sonica <shreya.boyane@gmail.com>
2024-09-09 23:31:21 -0400

    Assignment completed.

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -12,21 +12,25 @@
 
 import cs547
 import PorterStemmer
+import re
+import glob
+#import tokenize
 
-MY_NAME = "Your Name"
-MY_ANUM  = 0000000 # put your WPI numerical ID here
-MY_EMAIL = "somewhere@wpi.edu"
+import io
+
+MY_NAME = "Shreya Boyane"
+MY_ANUM  = 901029106 # put your WPI numerical ID here
+MY_EMAIL = "ssboyane@wpi.edu"
 
 # the COLLABORATORS list contains tuples of 2 items, the name of the helper
 # and their contribution to your homework
 COLLABORATORS = [ 
-    ('Bob Lee', 'helped me learn Python'),  
-    ('Brown Cheng', 'gave me coffee during office hours'),
+    ('Ankit Gole', 'helped me in code testing'),
     ]
 
 # Set the I_AGREE_HONOR_CODE to True if you agree with the following statement
 # "I do not lie, cheat or steal, or tolerate those who do."
-I_AGREE_HONOR_CODE = False
+I_AGREE_HONOR_CODE = True
 
 # this defines the student object
 student = cs547.Student(
@@ -65,7 +69,7 @@ class Index(object):
         #      }
 
 
-    # index_dir( base_path )
+    #index_dir( base_path ):
     # purpose: crawl through a nested directory of text files and generate an
     #   inverted index of the contents
     # preconditions: none
@@ -74,9 +78,30 @@ class Index(object):
     # parameters:
     #   base_path - a string containing a relative or direct path to a
     #     directory of text files to be indexed
+
+
+
+
     def index_dir(self, base_path):
         num_files_indexed = 0
         # PUT YOUR CODE HERE
+       # txt_files = glob.glob(base_path + '/**/*.txt', recursive=True)
+
+        for filepath in glob.glob(f'{base_path}/*.txt'):
+            self._documents.append(filepath)
+            with open(filepath, 'r', encoding='utf-8') as file:
+                text = file.read()
+
+                tokens = self.tokenize(text)
+                stemmed_tokens = self.stemming(tokens)
+                print(stemmed_tokens)
+                for token in stemmed_tokens:
+                    if token not in self._inverted_index:
+                        self._inverted_index[token] = set()
+                    self._inverted_index[token].add(len(self._documents)-1)
+
+                num_files_indexed += 1
+        print(self._inverted_index)
         return num_files_indexed
 
     # tokenize( text )
@@ -88,9 +113,7 @@ class Index(object):
     # parameters:
     #   text - a string of terms
     def tokenize(self, text):
-        tokens = []
-        # PUT YOUR CODE HERE
-        return tokens
+        return re.findall(r'\b\w+\b', text.lower())
 
     # purpose: convert a string of terms into a list of tokens.        
     # convert a list of tokens to a list of stemmed tokens,     
@@ -99,10 +122,8 @@ class Index(object):
     # parameters:
     #   tokens - a list of tokens
     def stemming(self, tokens):
-        stemmed_tokens = []
-        # PUT YOUR CODE HERE
-        return stemmed_tokens
-    
+        return [PorterStemmer.PorterStemmer().stem(token, 0, len(token) - 1) for token in tokens]
+
     # boolean_search( text )
     # purpose: searches for the terms in "text" in our corpus using logical OR or logical AND. 
     # If "text" contains only single term, search it from the inverted index. If "text" contains three terms including "or" or "and", 
@@ -113,9 +134,42 @@ class Index(object):
     # parameters:
     #   text - a string of terms
     def boolean_search(self, text):
-        results = []
         # PUT YOUR CODE HERE
-        return results
+        tokens = self.tokenize(text)
+        stemmed_tokens = self.stemming(tokens)
+
+        if len(stemmed_tokens) == 1:
+            return self.single_term(stemmed_tokens[0])
+        elif len(stemmed_tokens) == 3:
+            if stemmed_tokens[1] == 'and':
+                return self.single_term(stemmed_tokens[0])
+            elif stemmed_tokens[1] == 'or':
+                return self.OR(stemmed_tokens[0], stemmed_tokens[2])
+        else:
+            return []
+
+    def single_term(self, word):
+        if word in self._inverted_index:
+            return [self._documents[doc_id] for doc_id in self._inverted_index[word]]
+
+
+    def AND(self, word1, word2):
+        if word1 in self._inverted_index and word2 in self._inverted_index:
+            # Convert the lists to sets before intersecting
+            return [self._documents[doc_id] for doc_id in (set(self._inverted_index[word1]) & set(self._inverted_index[word2]))]
+        else:
+            return []
+
+    def OR(self, word1, word2):
+
+        docs = set()
+        if word1 in self._inverted_index:
+            docs.update(self._inverted_index[word1])
+        if word2 in self._inverted_index:
+            docs.update(self._inverted_index[word2])
+        return [self._documents[doc_id] for doc_id in docs]
+
+
     
 
 # now, we'll define our main function which actually starts the indexer and
